df938359bb9230094e1d94e21e7f836d9a00a0b5
michaelslain <misha.slain@icloud.com>
2020-11-08 10:19:14 -0800

    Github pages compatiable

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const __dirname = path.resolve(path.dirname(''))
 const port = process.env.PORT || 6969
 
 express()
-    .use(express.static('client'))
-    .use('/client/', express.static(path.join(__dirname, 'client')))
+    .use(express.static('docs'))
+    .use('/client/', express.static(path.join(__dirname, 'docs')))
     .get('*', (req, res) =>
-        res.sendFile(path.join(__dirname, 'client', 'index.html'))
+        res.sendFile(path.join(__dirname, 'docs', 'index.html'))
     )
     .listen(port, () => console.log(`bruh ${port}`))
